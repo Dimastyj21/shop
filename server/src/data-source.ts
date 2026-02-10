222275fac1fe2c1bcad8fa0ad88e50210f2f898e
Dimastyj21 <pandatulen2015@gmail.com>
2026-02-10 17:05:00 +0300

    created a table

---
+++ server/src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME || 'admin',
+  password: process.env.DB_PASSWORD || 'password123',
+  database: process.env.DB_DATABASE || 'shop',
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
